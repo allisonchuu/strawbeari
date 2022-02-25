5260232eef1a50ea070f120879e6fe034db75ab0
Allison Chu <allisonchu@Allisons-iMac.lan>
2022-02-24 22:43:03 -0800

    Added suprise panda gif

--- script.js
+++ script.js
@@ -3,3 +3,20 @@
 
 console.log("Hello, world!");
 
+
+var x = true;
+
+function moveGif() {
+    var elem2 = document.querySelector('.gifcontainer');
+
+    if(x == true){
+        elem2.classList.add('show-panda');
+        x = false;
+    }else{
+        elem2.classList.remove('show-panda');
+        x = true;
+    }
+}
+
+
+
