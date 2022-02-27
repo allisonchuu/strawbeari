1a2951813e1024d4e731f38820016b2a8b7f8938
Allison Chu <allisonchu@Allisons-iMac.lan>
2022-02-27 14:34:38 -0800

    Added side nav bar for mobile

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-console.log("Hello, world!");
+const openMenu = document.querySelector('#show-menu');
+const sideMenu = document.querySelector('#nav-menu');
 
 var elem2 = document.querySelector('.gifcontainer');
 var x = true;
+
+let navSec = document.querySelector('.nav-section ');
+
+openMenu.addEventListener('click', function() {
+    sideMenu.classList.add('active');
+});
+
+function hideMenu() {
+    sideMenu.classList.remove('active');
+}
+
+navSec.addEventListener("mouseover", function( event ) {
+  // highlight the mouseover target
+  event.target.style.color = "#bbb0b1";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
 
 function moveGif() {
     
