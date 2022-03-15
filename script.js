acd352c3d705d5b378af890632e97dfdb214315d
Allison Chu <allisonchu@Allisons-iMac.lan>
2022-03-15 03:09:03 -0700

    Cleaning up code

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
-const openMenu = document.querySelector('#show-menu');
 const sideMenu = document.querySelector('#nav-menu');
-
-var elem2 = document.querySelector('.gifcontainer');
+const gifContainer = document.querySelector('.gifcontainer');
 var x = true;
 
-let navSec = document.querySelector('.nav-section ');
-
-openMenu.addEventListener('click', function() {
+function showMenu() {
     sideMenu.classList.add('active');
-});
+}
 
 function hideMenu() {
     sideMenu.classList.remove('active');
 }
 
-navSec.addEventListener("mouseover", function( event ) {
-  // highlight the mouseover target
-  event.target.style.color = "#bbb0b1";
-
-  // reset the color after a short delay
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
-
 function moveGif() {
-    
-
     if(x == true){
-        elem2.classList.add('show-panda');
+        gifContainer.classList.add('show-panda');
         x = false;
     }else{
-        elem2.classList.remove('show-panda');
+        gifContainer.classList.remove('show-panda');
         x = true;
     }
 }
